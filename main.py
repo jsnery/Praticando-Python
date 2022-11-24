a2154dbf7425f5a602e8123d03fef1286cd968d4
Richard Neri <richardmatq@gmail.com>
2022-11-24 20:22:06 -0300

    Adicionando sistema de cadastro simples

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from resources import clear
 
-op = input(f'Escolha uma opção:\n\n1) Jogo da Forca:\n2) Impar ou Par\n\nDigite aqui: ')
+op = input(f'Escolha uma opção:\n\n1) Jogo da Forca:\n2) Impar ou Par\n3) Cadastro Simples\n\nDigite aqui: ')
 
 while True:
     try:
         intOp = int(op)
-        if intOp > 2 or intOp < 1:
+        if intOp > 3 or intOp < 1:
             clear()
             print('Opção Invalida!\n')
-            op = input(f'Escolha uma opção:\n\n1) Jogo da Forca:\n2) Impar ou Par\n\nDigite aqui: ')
+            op = input(f'Escolha uma opção:\n\n1) Jogo da Forca:\n2) Impar ou Par\n3) Cadastro Simples\n\nDigite aqui: ')
             continue
         break
     except ValueError:
         clear()
         print('Você não digitou um numero inteiro!\n')
-        op = input(f'Escolha uma opção:\n\n1) Jogo da Forca:\n2) Impar ou Par\n\nDigite aqui: ')
+        op = input(f'Escolha uma opção:\n\n1) Jogo da Forca:\n2) Impar ou Par\n3) Cadastro Simples\n\nDigite aqui: ')
 
 if op == '1':
     from games import hangmangame
@@ -22,3 +22,6 @@ if op == '1':
 elif op == '2':
     from matematica import impar_par
     impar_par
+elif op == '3':
+    from sistemas import cadastro_simples
+    cadastro_simples
---
+++ sistemas/cadastro_simples.py
@@ -0,0 +1,30 @@
+from resources import clear
+
+usersReg = []
+
+while True:
+    clear()
+    user = input('Digite seu usuário: ')
+    psw = input('Digite sua senha: ')
+
+    validation = user.isspace() or psw.isspace() or not bool(user) or not bool(psw)
+
+    while validation:
+        clear()
+        print('Usuáŕio e/ou Senha invalido(s)!\n')
+        user = input('Digite seu usuário: ')
+        psw = input('Digite sua senha: ')
+
+    register = {'user': user, 'psw': psw}
+    usersReg.append(register)
+
+    clear()
+    print('Lista de Registro:\n')
+    for i in usersReg:
+        print(f"Usuário:{i['user']}\nSenha:{i['psw']}\n")
+
+    op = input('Press [E] and [Enter] for Exit')
+
+    if op.lower() == 'e':
+        break
+    
