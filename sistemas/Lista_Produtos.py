e3820d6ca13c575dd1ac67929dc85dca5dafcd97
Richard Neri <richardmatq@gmail.com>
2022-11-26 01:54:02 -0300

    Revisado e Atualizado
    Refinando o processo... Ainda tem muito o que refinar, mas já está melhor que antes!

--- sistemas/Lista_Produtos.py
+++ sistemas/Lista_Produtos.py
@@ -1,85 +1,86 @@
 from copy import deepcopy
 from resources import clear
-import os, copy
+import os
 
 def ordenar(x):
-    if os.path.exists(NomeListaCompras):
-        os.remove(NomeListaCompras)
-    listaComprasNomes = copy.deepcopy(sorted(listaCompras, key=lambda i: i[x]))
-    listaComprasTXT = open(NomeListaCompras, 'a')
+    if os.path.exists(nomeLista_Txt):
+        os.remove(nomeLista_Txt)
+    listaComprasNomes = sorted(deepcopy(listaCompras), key=lambda i: i[x])
+    listaComprasTXT = open(nomeLista_Txt, 'a')
+    listaComprasTXT.write(f'{tituloLista_Txt}\n\n')
     for i in listaComprasNomes:
-        print(f"Produto: {i['produto']: ^15} | Unidades: {i['quantidade']:0>3} | Preço Unitário: {i['preco']:.2f} | Preço Total: {i['precoFinal']:.2f}")
-        listaComprasTXT.write(f"Produto: {i['produto']: ^15} | Unidades: {i['quantidade']:0>3} | Preço Unitário: {i['preco']:.2f} | Preço Total: {i['precoFinal']:.2f}\n")
+        print(f"Produto: {i['produto']: ^15} | Unidades: {i['quantidade']:0>3} | Preço Unitário: {i['preco']: ^8.2f} | Preço Total: {i['precoFinal']:,.2f}")
+        listaComprasTXT.write(f"Produto: {i['produto']: ^15} | Unidades: {i['quantidade']:0>3} | Preço Unitário: {i['preco']: ^8.2f} | Preço Total: {i['precoFinal']:,.2f}\n")
     listaComprasTXT.close()
 
 # Criando lista de compras ordenadas
 listaCompras = []
-op = None
+escolhaOpcao = None
+
 while True:
     clear()
-    produto = input('Digite o Nome do produto: ')
+    item = input('Digite o Nome do produto: ')
 
-    while True:
+    while True: # Insira a quantidade
         try:
-            quantidade = int(input('Digite a quantidade de compra: '))
+            qnt = int(input('Digite a quantidade de compra: '))
             break
         except ValueError as error:
             clear()
             print(f'{error.__class__.__name__}: A Quantidade deve ser um numero Interio\n')
 
-    while True:
+    while True: # Insira o Preço
         try:
-            preco = float(input('Digite o Preço do produto: R$'))
+            preco = float(input('Digite o Preço do produto: R$').replace(',', '.'))
             break
         except ValueError as error:
             clear()
             print(f'{error.__class__.__name__}: O Preço deve conter apenas numeros\n')
 
-    organizandoPorduto = {'produto': produto, 'quantidade': quantidade, 'preco': preco, 'precoFinal': float(preco)*int(quantidade)}
+    dictProdutos = {'produto': item, 'quantidade': qnt, 'preco': preco, 'precoFinal': preco*qnt}
 
-    listaCompras.append(organizandoPorduto)
+    listaCompras.append(dictProdutos)
 
-    op = input('\nDeseja continuar? [s]im ').lower()
+    escolhaOpcao = input('\nDeseja continuar? [s]im ').lower()
 
-    if op == 's':
+    if escolhaOpcao == 's':
         continue
     else:
         break
 
 clear()
-NomeListaCompras = input('Digite o Nome para a sua lista de compras: ')
-clear()
-NomeListaCompras += '.txt'
-listaComprasTXT = open(NomeListaCompras, 'a')
-for i in listaCompras:
-    print(f"Produto: {i['produto']: ^15} | Unidades: {i['quantidade']:0>3} | Preço Unitário: {i['preco']:.2f} | Preço Total: {i['precoFinal']:.2f}")
-    listaComprasTXT.write(f"Produto: {i['produto']: ^15} | Unidades: {i['quantidade']:0>3} | Preço Unitário: {i['preco']:.2f} | Preço Total: {i['precoFinal']:.2f}\n")
 
-listaComprasTXT.close()
+nomeLista_Txt = input('Nomeie o arquivo: ').replace(' ', '-')
+tituloLista_Txt = input('Digite o Titulo da lista: ')
+
+clear()
+
+nomeLista_Txt += '.txt'
+lista_Txt = open(nomeLista_Txt, 'a')
+lista_Txt.write(f'{tituloLista_Txt}\n\n')
+for i in listaCompras:
+    print(f"Produto: {i['produto']: ^15} | Unidades: {i['quantidade']:0>3} | Preço Unitário: {i['preco']: ^8.2f} | Preço Total: {i['precoFinal']:,.2f}")
+    lista_Txt.write(f"Produto: {i['produto']: ^15} | Unidades: {i['quantidade']:0>3} | Preço Unitário: {i['preco']: ^8.2f} | Preço Total: {i['precoFinal']:,.2f}\n")
+
+lista_Txt.close()
+
 print('\nLista de Compras Gerada com sucesso!\n')
 
-op = input('Deseja oredena-la? [s]im ').lower()
+escolhaOpcao = input('Deseja oredena-la? [s]im ').lower()
 
-if op == 's':
+if escolhaOpcao == 's':
     clear()
-    op = input('Escolha uma opção:\n\n1) Nome\n2) Quantidade\n3) Preço\n4) Preço Final\n\nDigite aqui: ')
+    escolhaOpcao = input('Escolha uma opção:\n\n1) Nome\n2) Quantidade\n3) Preço\n4) Preço Final\n\nDigite aqui: ')
 
-    while not op.isnumeric():
+    while not escolhaOpcao.isnumeric() or int(escolhaOpcao) > 4 or int(escolhaOpcao) < 0:
         clear()
-        op = input('Opçõa invalida!\n\nEscolha uma opção:\n\n1) Nome\n2) Quantidade\n3) Preço\n4) Preço Final\n\nDigite aqui: ')
+        escolhaOpcao = input('Opçõa invalida!\n\nEscolha uma opção:\n\n1) Nome\n2) Quantidade\n3) Preço\n4) Preço Final\n\nDigite aqui: ')
     
-    while int(op) > 4 or int(op) < 0:
-        clear()
-        op = input('Opçõa invalida!\n\nEscolha uma opção:\n\n1) Nome\n2) Quantidade\n3) Preço\n4) Preço Final\n\nDigite aqui: ')
-
-    if int(op) == 1:
+    if int(escolhaOpcao) == 1:
         ordenar('produto')
-    elif int(op) == 2:
+    elif int(escolhaOpcao) == 2:
         ordenar('quantidade')
-    elif int(op) == 3:
+    elif int(escolhaOpcao) == 3:
         ordenar('preco')
-    elif int(op) == 4:
+    elif int(escolhaOpcao) == 4:
         ordenar('precoFinal')
-
-else:
-    print('Bye Bye')
