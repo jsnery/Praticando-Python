fcd7eefb1bf2e8571fa6be19fe7cb5be410909a6
Richard Neri <richardmatq@gmail.com>
2022-12-10 14:09:53 -0300

    Update login_simples.py

--- sistemas/login_simples.py
+++ sistemas/login_simples.py
@@ -7,6 +7,7 @@ def login_system():
     password = None
     data_users_create = None
     list_data_users = []
+    validation = False
 
     with FILEDIR.open('r') as file:
         user_data = file.readlines()
@@ -15,12 +16,20 @@ def login_system():
             username, password = dados.split()
             data_users_create = {'user': username, 'psw': password}
             list_data_users.append(data_users_create)
+    
+    while True:
+        
+        match validation:
+            
+            clear()
+            user = input('Digite seu usuário: ')
+            password = input('Digite sua senha: ')
+            validation = user.isspace() or password.isspace() or not bool(user) or not bool(password)
+            
+            case True:
+                ...
+        
 
-    clear()
-    user = input('Digite seu usuário: ')
-    password = input('Digite sua senha: ')
-
-    validation = user.isspace() or password.isspace() or not bool(user) or not bool(password)
 
     while validation:
         clear()
