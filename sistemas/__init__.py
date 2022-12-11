9d6a0785e149d002bc0a837c7c080eddd42042d3
Richard Neri <richardmatq@gmail.com>
2022-12-10 23:59:39 -0300

    A

--- sistemas/__init__.py
+++ sistemas/__init__.py
@@ -2,6 +2,6 @@ from sistemas.cadastro_simples import registration_system
 from sistemas.login_simples import login_system
 from sistemas.gerador_cpf import cpf_generator
 from sistemas.validador_cpf import cpf_validator
-from sistemas.Lista_Produtos import product_list
+from sistemas.lista_produtos import product_list
 
 
