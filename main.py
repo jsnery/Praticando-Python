6599b67c8f605b59999cdc9902352284544aa823
Richard Neri <richardmatq@gmail.com>
2022-12-12 02:07:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from resources import clear
-from sistemas import registration_system, cpf_generator, product_list, login_system, cpf_validator
+from sistemas import registration_system, cpf_generator, product_list, login_system, cpf_validator, registration_login_sys
 from matematica import imparpar
 from games import hangman_game
 
-options_msg = f'Escolha uma opção:\n\n1) Jogo da Forca:\n2) Impar ou Par\n\
+options_msg = f'Escolha uma opção (Para [s]air):\n\n1) Jogo da Forca:\n2) Impar ou Par\n\
 3) Cadastro Simples\n4) Tela de Login\n5) Validador de CPF\n\
-6) Gerador de CPF\n7) Gerador de Lista de Compras\n\nDigite aqui: '
+6) Gerador de CPF\n7) Gerador de Lista de Compras\n8) Cadastro e Login com Decorator\n\nDigite aqui: '
        
 while True:
     op = input(options_msg).lower()
@@ -25,6 +25,8 @@ while True:
             cpf_generator()
         case '7':
             product_list()
+        case '8':
+            registration_login_sys()
         case 'sair' | 's' | 'e' | 'exit':
             break
         case _:
