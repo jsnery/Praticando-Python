7b88d7574d7131bb181119196d1a2f291fcdcf23
Richard Neri <richardmatq@gmail.com>
2022-12-12 02:07:49 -0300

    Update __init__.py

--- sistemas/__init__.py
+++ sistemas/__init__.py
@@ -3,5 +3,6 @@ from sistemas.login_simples import login_system
 from sistemas.gerador_cpf import cpf_generator
 from sistemas.validador_cpf import cpf_validator
 from sistemas.lista_produtos import product_list
+from sistemas.registrationlogin import registration_login_sys
 
 
